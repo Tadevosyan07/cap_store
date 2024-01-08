c153783ad16f43159a1faf97e4a1fc4d926dbaf7
Tadevossian <tadevosyanhayk07@gmail.com>
2024-01-08 14:46:14 +0400

    second commit

--- Backend/Service/seedService.js
+++ Backend/Service/seedService.js
@@ -7,15 +7,10 @@ const seedService = {
   seedDatabase: async () => {
     console.log(3);
     try {
-      console.log(4)
       await Cap.deleteMany({})
-      console.log(5)
       await Blog.deleteMany({})
-      console.log(6)
       const newCaps = await Cap.insertMany(data.caps)
-      console.log(7)
       const newBlog = await Blog.insertMany(data.blog)
-      console.log(8);
       return {
         message: 'Data created',
         data: {
--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connection from "./Utils/conntection.js";
 import seedRouter from "./Router/SeedRoute.js";
-import capRouter from "./Router/capRouter.js"
-import blogRouter from "./Router/blogRouter.js"
+import capRouter from "./Router/capRouter.js";
+import blogRouter from "./Router/blogRouter.js";
 import cartRouter from "./Router/CartRouter.js";
 import Cart from "./Model/CartModel.js";
 
@@ -13,10 +13,10 @@ connection();
 
 app.use(express.json());
 
-app.use("/api/database",seedRouter);
-app.use("/api/caps", capRouter)
-app.use("/api/blog", blogRouter)
-app.use("/api/cart", cartRouter)
+app.use("/api/database", seedRouter);
+app.use("/api/caps", capRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/cart", cartRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${process.env.PORT}`);
